b11670cf1c0c90f73fc7e3643336089fe492192f
yysunq <yysunq@gmail.com>
2022-10-28 12:06:46 +0800

    add mv process

--- train.sh
+++ train.sh
@@ -5,6 +5,12 @@ then
         python_cmd="python"
 fi
 
+if [[ -z "${desc}" ]]
+then
+        desc="noveai-pruned"
+fi
+
+
 if [[ -z "${base_yaml}" ]]
 then
         base_yaml="./configs/stable-diffusion/v1-finetune_unfrozen.yaml"
@@ -16,12 +22,12 @@ then
 fi
 if [[ -z "${reg_data_root}" ]]
 then
-        reg_data_root="/root/autodl-tmp/dreambooth/Dreambooth-Stable-Diffusion/regularization_images/man"
+        reg_data_root="/root/autodl-tmp/dreambooth/Dreambooth-Stable-Diffusion/regularization_images/costume"
 fi
 
 if [[ -z "${proj_name}" ]]
 then
-        proj_name="tlbbv6"
+        proj_name="tlbbv7"
 fi
 
 if [[ -z "${train_data}" ]]
@@ -36,7 +42,7 @@ fi
 
 if [[ -z "${class_word}" ]]
 then
-        class_word="man"
+        class_word="costume"
 fi
 
 if [[ -z "${token}" ]]
@@ -46,3 +52,17 @@ fi
 
 #`python ./main.py --base ./configs/stable-diffusion/v1-finetune_unfrozen.yaml -t --actual_resume anime700k-64bs-0.1ucg-penultimate-1epoch-clip-ema-continue-76000-ema-pruned.ckpt --reg_data_root /root/autodl-tmp/dreambooth/Dreambooth-Stable-Diffusion/regularization_images/female -n tlbbv6 --gpu 0, --data_root /root/autodl-tmp/dreambooth/Dreambooth-Stable-Diffusion/training_images --max_training_steps 2000 --class_word female --token tlbb --no-test`
 `${python_cmd} ./main.py --base ${base_yaml} -t --actual_resume ${actual_resume} --reg_data_root ${reg_data_root} --name ${proj_name} --gpu 0, --data_root ${train_data} --max_training_steps ${steps} --class_word ${class_word} --token ${token} --no-test`
+
+filelist=(`ls -dtr logs/*`)
+lastidx=${#filelist[*]}
+echo "lastidx = $lastidx"
+lastdir=${filelist[$lastidx-1]}
+ckptpath="${lastdir}/checkpoints/last.ckpt"
+echo "source ckpt path = ${ckptpath}"
+time=$(date "+%Y%m%d-%H%M%S")
+echo $time
+finalpath="trained_models/${time}-${proj_name}-token(${token})-class_word(${class_word})-steps(${steps})-desc(${desc}).ckpt"
+echo "dest ckpt path = ${finalpath}"
+mv $ckptpath $finalpath
+echo "success mv ${finalpath}!!!"
+
